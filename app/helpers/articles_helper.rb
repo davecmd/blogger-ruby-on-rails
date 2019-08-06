1e9168d8715380349bb1df5da1b3e00366bded47
David Tan <dave.tan.work@gmail.com>
2019-08-06 06:56:11 -0500

    added a few gems

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,4 @@
 module ArticlesHelper
     def article_params
-        params.require(:article).permit(:title, :body, :tag_list)
-      end
+      params.require(:article).permit(:title, :body, :tag_list, :image)      end
 end
